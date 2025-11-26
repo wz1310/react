c8fec3ee3a966c48465293aca3672bf968e38f74
wz1310 <a002041zano@gmail.com>
2025-11-26 13:22:12 +0700

    update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { SERVICES_CONTENT } from '../constants'
 
 const Services = () => {
   return (
     <section className='max-w-7xl mx-auto border-b-2' id="services">
         <div className='my-20'>
-            <h2>Our Home Renovation Services</h2>
+            <h2 className='text-xl lg:text-3xl tracking-tight text-center uppercase mb-20'>
+            {SERVICES_CONTENT.map((services,index)=>(
+              <div key={index} className='mb-12 mx-4 flex flex-col lg:flex-row'>
+                <div className={`lg:w-1/2 mb-4 lg:mb-0 ${
+                  index%2 === 0 ? "":"lg:order-2 "
+                }`}>
+                  <img src={services.image} alt={services.title}
+                  className='w-full h-auto object-cover rounded-lg '></img>
+                </div>
+              </div>
+            ))}</h2>
         </div>
     </section>
   )
